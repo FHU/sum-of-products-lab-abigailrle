fc98e2ce93eb81c81027a0ca1857d5ba4ce40d8c
Abigail <abigail.leland@students.fhu.edu>
2024-02-01 21:37:06 -0600

    main.py
    removing extra error codes and text

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 def sum_of_products(list1, list2):
     if len(list1) == len(list2):
-        if list1.isdigit() and list2.isdigit():
-            length = len(list1)
-            index = 0
-            number_total = 0
-            while index < length:
-                number = int(list1[index]) * int(list2[index])
-                number_total = number_total + number
-                index = index + 1
-            print(number_total)
-        else:
-            print('Error: you must only input integers')
+        #if list1.isdigit() and list2.isdigit():
+        length = len(list1)
+        index = 0
+        number_total = 0
+        while index < length:
+            number = int(list1[index]) * int(list2[index])
+            number_total = number_total + number
+            index = index + 1
+        print(number_total)
+        #else:
+            #print('Error: you must only input integers')
     else:
-        print('Error: numbers must be the same length')
+        print('Error') #: numbers must be the same length
 
 
 
 if __name__ == '__main__':
-    input_list1 = input('type the first number')
-    input_list2 = input('type the second number')   
+    input_list1 = input() #'type the first number'
+    input_list2 = input() #'type the second number'   
     sum_of_products(input_list1, input_list2)  
