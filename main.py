2555c8765e3bd9425bf4df1baff57f0522eacc12
Abigail <abigail.leland@students.fhu.edu>
2024-02-06 12:25:29 -0600

    main.py
    remove if statements

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 def sum_of_products(list1, list2):
-    if len(list1) == len(list2):
-        list1 = list1.split()
-        list2 = list2.split()
-        #if list1.isdigit() and list2.isdigit():
-        length = len(list1)
-        index = 0
-        number_total = 0
-        while index < length:
-            number = int(list1[index]) * int(list2[index])
-            number_total = number_total + number
-            index = index + 1
-        return number_total
+    #if len(list1) == len(list2):
+    list1 = list1.split()
+    list2 = list2.split()
+    #if list1.isdigit() and list2.isdigit():
+    length = len(list1)
+    index = 0
+    number_total = 0
+    while index < length:
+        number = int(list1[index]) * int(list2[index])
+        number_total = number_total + number
+        index = index + 1
+    return number_total
         #else:
          #     print('Error')
          #   index = 0 #pass #rint('Error: you must only input integers')
-    else:
-        return 'Error' #: numbers must be the same length
+    #else:
+     #   return 'Error' #: numbers must be the same length
 #yo
 
 
