80b510ff3af2c6e03d15b55a9e304f87afedf603
Abigail <abigail.leland@students.fhu.edu>
2024-02-06 12:12:13 -0600

    main.py
    updates

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def sum_of_products(list1, list2):
     if len(list1) == len(list2):
-        list1 = list1.split(' ')
-        list2 = list2.split(' ')
+        list1 = list1.split()
+        list2 = list2.split()
         #if list1.isdigit() and list2.isdigit():
         length = len(list1)
         index = 0
@@ -10,16 +10,16 @@ def sum_of_products(list1, list2):
             number = int(list1[index]) * int(list2[index])
             number_total = number_total + number
             index = index + 1
-        print(number_total, '\r\n')
+        return number_total
         #else:
          #     print('Error')
          #   index = 0 #pass #rint('Error: you must only input integers')
     else:
-        print('Error') #: numbers must be the same length
+        return 'Error' #: numbers must be the same length
 #yo
 
 
 if __name__ == '__main__':
     input_list1 = input() #'type the first number'
     input_list2 = input() #'type the second number'   
-    sum_of_products(input_list1, input_list2)  
+    print(sum_of_products(input_list1, input_list2))
